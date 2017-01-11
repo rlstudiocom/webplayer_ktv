cee5a4f51e4c3f849b4c8108b6b1868d0a15c127
dk <you@example.com>
2017-01-12 01:08:06 +0400

    added new player. test

--- src/main.js
+++ src/main.js
@@ -5,3 +5,9 @@ new Vue({
     el: '#app',
     render: h => h(App)
 })
+
+/*создали плеер */
+var player = KartinaPlayerFactory({server_url: "https://rustvt.kartina.tv/", autologin: true});
+/*запустили*/
+player.run();
+
